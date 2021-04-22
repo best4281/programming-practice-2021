66fed23b3908692a46abf161ef970efed2376ab7
Thitisan Sihanern <67893680+best4281@users.noreply.github.com>
2021-04-23 00:45:55 +0900

    Exercise 1 - Submission 1
    I have done task 2,3,4, and 6.
    For task 1, I understand how to make a natural error, but I don't know how to create (inside Person class) an abstract method that detect if it git overridden or not.
    For task 5, I have no idea how to do it when user input is not required.

--- exercise_1/exercise_1.py
+++ exercise_1/exercise_1.py
@@ -1,3 +1,102 @@
+from abc import ABC, abstractmethod
+
+#Example input
+p = ['Teacher Andy', 'Teacher Blake', 'Teacher Canon']
+q = ['Student Alex', 'Student Bethany', 'Student Carlson', 'Student Daniel']
+r = {
+    'l101': ['Teacher Blake', ['Student Alex', 'Student Bethany']], 
+    'l102': ['Teacher Andy', ['Student Bethany', 'Student Carlson']],
+    'l103': ['Teacher Canon', ['Student Bethany', 'Student Alex']],
+    'l104': ['Teacher Andy', ['Student Bethany', 'Student Daniel']],
+    'l105': ['A',['B']]
+}
+inp = [p, q, r]
+
+#Task 6 - done
+studentnum = 1000
+teachernum = 2000
+
+#Task 1 - without natural error
+class Person(ABC):
+
+    name = ''
+
+    @abstractmethod
+    def get_attributes(self):
+        pass
+
+#Task 2.1 - done
+class Student(Person):
+
+    def __init__(self, name):
+        self.name = name
+        self.lecture_enrolled = set()
+        #Task 6.1 - assigning student_id on the go
+        global studentnum
+        studentnum += 1
+        self.student_id = "T" + str(studentnum)
+
+    def get_attributes(self):
+        try:
+            return { student_id : lecture_enrolled }
+        except:
+            return
+
+#Task 2.2 - done
+class Teacher(Person):
+
+    def __init__(self, name):
+        self.name = name
+        self.lecture_taught = ""
+        #Task 6.2 - assigning teacher_id on the go
+        global teachernum
+        teachernum += 1
+        self.teacher_id = "T" + str(teachernum)
+    
+    def get_attributes(self):
+        try:
+            return { teacher_id : lecture_taught }
+        except:
+            return
+
+#Task 3 - error with attribute "students"
+class Lecture:
+
+    def __init__(self, lecid):
+        self.lecture_id = lecid
+        self.students = set()
+
+    def assign_teacher(self, lecturer):
+        if lecturer.lecture_taught == "":
+            self.lecturer = lecturer
+            lecturer.lecture_taught = self.lecture_id
+        else:
+            #Task 4.1 - done
+            print(lecturer.name, "cannot be assigned to teach", self.lecture_id)
+            print("Reason:", lecturer.name, "is already assigned to", lecturer.lecture_taught, "\n")
+
+    def get_teacher(self):
+        #print("Lecturer name:",self.lecturer)
+        try:
+            return self.lecturer.name
+        except:
+            return "no lecturer assigned."
+    
+    def assign_students(self, stu):
+        if len(stu.lecture_enrolled) < 3:
+            self.students.add(stu)
+            stu.lecture_enrolled.add(self.lecture_id)
+        else:
+            #Task 4.2 - done
+            print(stu.name, "cannot enroll to", self.lecture_id)
+            print("Reason:", stu.name, "already enrolled to maximum of 3 lectures\n")
+    
+    def get_students(self):
+        if len(self.students) != 0:
+            return self.students
+        else:
+            return False
+
 def exercise_1(inputs): # DO NOT CHANGE THIS LINE
     """
     This functions receives the input in the parameter 'inputs'. 
@@ -25,6 +124,57 @@ def exercise_1(inputs): # DO NOT CHANGE THIS LINE
         6: [Teacher, Student, t, s]
     }
     """
-    output = inputs
+    #Creating dictionary of instances of student, with students' name as keys
+    stu = {}
+    for x in inputs[1]:
+        stu[x] = Student(x)
+
+    #Creating dictionary of instances of teacher, with teachers' name as keys
+    tea = {}
+    for x in inputs[0]:
+       tea[x] = Teacher(x)
+
+    #Creating dictionary of instances of lecture, with lecture ID as keys
+    lec = {}
+    for x in inputs[2]:
+        lec[x] = Lecture(x)
+        try:
+           lec[x].assign_teacher(tea[inputs[2][x][0]])
+        except:
+            print("No teacher with name", inputs[2][x][0], "in this system")
+        for y in inputs[2][x][1]:
+            try:
+                lec[x].assign_students(stu[y])
+            except:
+                print("No student with name", y, "in this system")
+
+    #Code for debugging each variable (stu,tea,lec)
+    '''print()
+    for x in lec:
+        print("Lecture code",lec[x].lecture_id, "has", lec[x].get_teacher())
+        print("Lecture participant(s):")
+        if not lec[x].get_students():
+            print("No student enrolled to this lecture.")
+        else:
+            for y in lec[x].get_students():
+                print(">", y.name)
+        print()
+
+    print()
+    for x in stu:
+        print(stu[x].name, "has enrolled in lecture:", *stu[x].lecture_enrolled)
+
+    print()
+    for x in tea:
+        print(tea[x].name, "is in charge of lecture", tea[x].lecture_taught)'''
+ 
+    output = {
+        1: [Person], 
+        2: [Teacher, Student, tea, stu], 
+        3: [Lecture, lec], 
+        4: [Lecture, lec], 
+
+        6: [Teacher, Student, tea, stu]
+    }
 
     return output       # DO NOT CHANGE THIS LINE
